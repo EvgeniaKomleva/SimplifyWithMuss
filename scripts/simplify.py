24aa7ccece98bf9f2980ef1255c8ffcd6455962b
EvgeniaKomleva <36705500+EvgeniaKomleva@users.noreply.github.com>
2021-06-14 14:50:15 +0300

    Update simplify.py

--- scripts/simplify.py
+++ scripts/simplify.py
@@ -1,18 +1,23 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import argparse
+from deep_translator import GoogleTranslator
+import time 
+import random
 
 from muss.simplify import ALLOWED_MODEL_NAMES, simplify_sentences
 from muss.utils.helpers import read_lines
 
+f_en = open("en_input.txt", "w+")
+f_out = open("output.txt", "w+")
+def safe_translate(translator, text):
+        text = translator.translate(text)
+        time.sleep(0.5 + random.random())
+        return text
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description='Simplify a file line by line.')
-    parser.add_argument('filepath', type=str, help='File containing the source sentences, one sentence per line.')
+    parser.add_argument('--filepath',
+     type=str,default="ru_input.txt" ,help='File containing the source sentences, one sentence per line.')
     parser.add_argument(
         '--model-name',
         type=str,
@@ -21,9 +26,18 @@ if __name__ == '__main__':
         help=f'Model name to generate from. Models selected with the highest validation SARI score.',
     )
     args = parser.parse_args()
-    source_sentences = read_lines(args.filepath)
+    f = open(args.filepath, "r")
+    content = f.readlines()
+    source_sentences = []
+    for sent in content:
+      transated = safe_translate(GoogleTranslator(source='ru', target='en'), sent)
+      source_sentences.append(transated)
+    print("translation ready!")
+
+    #source_sentences = read_lines("en_input.txt")
     pred_sentences = simplify_sentences(source_sentences, model_name=args.model_name)
-    for c, s in zip(source_sentences, pred_sentences):
-        print('-' * 80)
-        print(f'Original:   {c}')
-        print(f'Simplified: {s}')
+    for c, s in zip(source_sentences, pred_sentences):        
+        answer = safe_translate(GoogleTranslator(source='en', target='ru'), s)
+        print(answer)
+        f_out.write(answer)
+        f_out.write('\n')
